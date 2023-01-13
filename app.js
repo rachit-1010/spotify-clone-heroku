559e8eb41f63937573f8407aa7edf153798d2c96
unknown <shahrm@uchicago.edu>
2023-01-13 14:35:54 -0600

    minor changes

--- app.js
+++ app.js
@@ -8,9 +8,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const { json } = require('express');
 mongoose.set('strictQuery', false);
+require('dotenv').config();
 
 const hostname = '127.0.0.1';
-const port = 80;
+const port = process.env.PORT || 80;
 
 app.use(express.static('static'));
 // app.use(express.urlencoded());
@@ -36,16 +37,12 @@ app.post("/findSong", (req, res)=>{
     console.log(query);
     // res.sendFile(path.join(__dirname, '/static/searchSongList.html'));
 
-    const params = {key: 'AIzaSyDgGYLTzUbC1Y9BGiYdvUq6wCuwoV0q5xw', q: query, part: 'snippet', maxResults: 6};
+    const params = {key: process.env.YTAPI_KEY, q: query, part: 'snippet', maxResults: 6};
     axios.get('https://www.googleapis.com/youtube/v3/search', {params})
         .then(response => {
-            console.log(response.data.items[0]['id']['videoId']);
             let url_arr = [];
             for (let index = 0; index < 6; index++) {
-                // console.log("Inside for" + index);
-                // console.log(response.data.items[index]);
-                // console.log(response.data.items[index]['id']['videoId']);
-            url_arr.push("https://www.youtube.com/embed/" + response.data.items[index]['id']['videoId'] + "?enablejsapi=1&version=3&playerapiid=ytplayer");
+                url_arr.push("https://www.youtube.com/embed/" + response.data.items[index]['id']['videoId'] + "?enablejsapi=1&version=3&playerapiid=ytplayer");
             }
             console.log(url_arr[0]);
             const paramsSearchSongList = {'title': query, 'ytsearchdata': response, 'urls': url_arr};
@@ -60,18 +57,18 @@ app.get("/addSong", (req, res)=>{
     res.sendFile(path.join(__dirname, '/static/addSong.html'));
 });
 
-mongoose.connect('mongodb://127.0.0.1:27017/rachit');
+const mongoDB_URL = process.env.MONGODBURL;
+mongoose.connect(mongoDB_URL);
 console.log("Connected to database rachit");
 const songSchema = mongoose.Schema({'title': String,'album': String, 'duration': String, "videoId": String});
 const songModel = mongoose.model('song', songSchema);
-console.log("----------------");
-console.log("Here are all the songs");
+
 
 
 app.get("/submit", (req, res) => {
 
         console.log(req.query);
-        const firstSong = new songModel({'title': req.query.myName, 'album': req.query.album, 'duration': '4:05', 'videoId': req.query.videoId});
+        const firstSong = new songModel({'title': req.query.myName, 'album': req.query.album, 'duration': req.query.duration, 'videoId': req.query.videoId});
 
         firstSong.save();
 
@@ -85,9 +82,7 @@ app.get("/api/getAllSongs", (req, res) => {
         if (err){
             console.log(err);
         }
-        else{
-            console.log(data);
-        }
+        
 
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify({data}));
